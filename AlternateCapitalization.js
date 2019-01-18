bbe6e4a8330252cc28ffab439465b6f7f956fb5f
Debjani Paul <debjanideb@hotmail.com>
2019-01-17 20:18:43 -0800

    second commit

--- AlternateCapitalization.js
+++ AlternateCapitalization.js
@@ -3,33 +3,23 @@
 // For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']
 
 
-function capitalize(s){
+function capitalize(s) {
 
-   	var eveUpper = "";
-  	var oddUpper = "";
-  
-  	for(var i = 0; i< s.length; i++){
-   		if(i % 2 === 0){
-     		eveUpper += s[i].toUpperCase();
-   	  
-   	}else {
-            eveUpper+= s[i];
-         }        
-         
-  	}//End of for loop
+    var eveUpper = "";
+    var oddUpper = "";
 
-  
-  	for( i = 0; i< s.length; i++){
-  		if(i % 2 !== 0){
-    
-    		 oddUpper += s[i].toUpperCase();
-     
-   	}else {
-            oddUpper+= s[i];
-         }  
-  	}//End of for loop
+    for (var i = 0; i < s.length; i++) {
 
-	return [eveUpper, oddUpper]; //return in an array
+        if (i % 2 === 0) {
+            eveUpper += s[i].toUpperCase();
+            oddUpper += s[i];
+        } else {
+            eveUpper += s[i];
+            oddUpper += s[i].toUpperCase();
+        }
+    } //end of for loop
+
+    console.log([eveUpper, oddUpper]); //return in an array
 }
 
 capitalize("abcdef");
