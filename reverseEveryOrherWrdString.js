16c0e0c6147dcefc7e8899e1f3a0b2c47bac8199
Debjani Paul <debjanideb@hotmail.com>
2020-03-29 14:41:34 -0700

    Solvd Challenge

---
+++ reverseEveryOrherWrdString.js
@@ -0,0 +1,21 @@
+/* Reverse every other word in a given string, then return the string. 
+Throw away any leading or trailing whitespace, while ensuring there is 
+exactly one space between each word. Punctuation marks should be treated 
+as if they are apart of the word in this kata. */
+
+function reverse(str) {
+    let a = str.split(' ')
+
+    for (let i = 0; i < a.length; i++) {
+        if (i % 2 !== 0) {
+            let b = a[i].split('').reverse().join('');
+            str = str.replace(a[i], b);
+        }
+
+    }
+    return (str.trim())
+}
+
+reverse("Did it work?");
+reverse("I really hope it works this time...");
+reverse("Reverse this string, please!"),
